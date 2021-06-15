8114ef7fcde5e17397dbc8ee7a3bc5f1c6e09adf
xand974 <xand974@gmail.com>
2021-06-16 00:21:05 +0200

    rajout vue Create + validation avec model

--- AspAppMvcWithDb/ViewModels/CreateViewModel.cs
+++ AspAppMvcWithDb/ViewModels/CreateViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using AspAppMvcWithDb.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspAppMvcWithDb.ViewModels
 {
     public class CreateViewModel
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Minimum 1 charactère s'il vous plaît !")]
+        [MaxLength(40, ErrorMessage = "Maximum 40 charactère s'il vous plaît !")]
         public string Title { get; set; }
+
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Minimum 1 charactère s'il vous plaît !")]
         public string Description { get; set; }
+
+
         public Creator Creator { get; set; }
     }
 }
