fdcbe2c8649dd6865008cd78fb210136b7fb8e9a
xand974 <xand974@gmail.com>
2021-06-20 23:36:26 +0200

    ajout fonctionnalité : Register

---
+++ AspAppMvcWithDb/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using AspAppMvcWithDb.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspAppMvcWithDb.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> manager;
+        private readonly ILogger<UserController> logger;
+
+        public UserController(UserManager<IdentityUser> manager ,
+                              SignInManager<IdentityUser> signInManager,
+                              ILogger<UserController> logger )
+        {
+            this.manager = manager;
+            SignInManager = signInManager;
+            this.logger = logger;
+        }
+
+        public SignInManager<IdentityUser> SignInManager { get; }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new()
+                {
+                    UserName = model.Username
+                };
+
+                //Creation user dans la DB + hash password
+                var result = await manager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    //si on réussit à créer un compte, alors on l'authentifie directement en envoyant un cookie 
+                    await SignInManager.SignInAsync(user, isPersistent: true) ;
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    logger.LogError(error.Description, error.Code);
+
+                    //va retourner les erreurs directement dans la View
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ AspAppMvcWithDb/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspAppMvcWithDb.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name ="Pseudo")]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name ="Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirmation de mot de passe")]
+        [Compare("Password", ErrorMessage ="Votre mot de passe doit correspondre")]
+        public string ConfirmPassword { get; set; }
+    }
+}
